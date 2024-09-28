7eb26ff6c55b6a2db638909b5628439f88cb3101
karan <kkadam58707@gmail.com>
2024-09-28 07:29:30 +0530

    lal

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/lal/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['firebase/auth','firebase/firestore','react-firebase-hooks/auth','firebase/app']
+    }
+  }
+});
